028ab66583f2ee7f4fbf9aaaaba64acb35fa7a09
[joshua nduka] <[ndukajoshua6@gmail.com]>
2026-03-03 08:46:36 +0100

    J

--- app/(auth)/verifyemail.tsx
+++ app/(auth)/verifyemail.tsx
@@ -1,12 +1,32 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function VerifyEmail() {
   return (
-    <View>
-      <Text>verifyemail</Text>
-    </View>
-  )
+    <SafeAreaView style={styles.container}>
+
+      
+   
+    </SafeAreaView>
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal:16,
+    backgroundColor: '#fff', 
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: '700',
+    marginBottom: 10,
+    color: '#333',
+  },
+  subtitle: {
+    fontSize: 16,
+    color: '#555',
+    textAlign: 'center',
+  },
+});
